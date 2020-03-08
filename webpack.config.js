541ef5eb11209d9b390c16c11c2de54d3852cb61
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-03-08 15:01:47 +0700

    init react

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: "/dist/"
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          preset: ['@babel/preset-env', '@babel/preset-react']
-        }
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        include: path.resolve(__dirname, 'src'),
+        use: ['babel-loader'],
       }
     ]
   },
-  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })]
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 3000,
+    publicPath: "http://localhost:3000",
+    open: true,
+    stats: "errors-only",
+    overlay: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ]
 }

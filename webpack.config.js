a5acbadbe9665d0357c1168f95685cd417d7f6ad
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-03-08 15:12:35 +0700

    change js to jsx

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index'),
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -22,6 +22,9 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }
     ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
